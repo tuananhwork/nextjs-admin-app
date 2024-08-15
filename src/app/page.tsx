42730029052425ce32c3911073895ae4c001c2f0
Tuan Anh <chubatuananh.work@gmail.com>
2024-08-15 17:18:18 +0700

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+
+
+export default function Home() {
+  return (
+    <div>Hello anh em
+      <div>
+        <Button type="primary">Click</Button>
+      </div>
+    </div>
+  );
+}
